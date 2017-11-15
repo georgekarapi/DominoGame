75ff024d37801a648c7533e27d70de4d8c1b6983
gkarapii <georgekarapi@yahoo.gr>
2017-11-15 18:03:42 +0200

    Added TilesGroup return.

--- src/Domino/Base/Dominoes.java
+++ src/Domino/Base/Dominoes.java
@@ -15,21 +15,34 @@ public class Dominoes {
             }
         }
     }
-    public Tile giveTile(int num){
+    public Tile giveTile(){
         Random r = new Random();
         int tile = r.nextInt(tiles.size() - 1);
         Tile tCopy = new Tile(tiles.get(tile).getLeft(), tiles.get(tile).getRight());
         tiles.remove(tile);
         return tCopy;
     }
+    public ArrayList<ArrayList<Tile>> getGroupofTiles(int HEIGHT, int WIDTH){
+        Tile tile;
+        ArrayList<ArrayList<Tile>> tiles = new ArrayList<>();
+        for(int i = 0; i < HEIGHT; i++){
+            ArrayList<Tile> tileY = new ArrayList<>();
+            for(int j = 0; j < WIDTH; j++){
+                tile = giveTile();
+                tileY.add(tile);
+            }
+            tiles.add(tileY);
+        }
+        return tiles;
+    }
     public static void main(String[] args) {
         Dominoes dom = new Dominoes();
         Tile x;
-        x = dom.giveTile(1);
+        x = dom.giveTile();
         System.out.println(x.getLeft()+","+x.getRight()) ;
-        x = dom.giveTile(1);
+        x = dom.giveTile();
         System.out.println(x.getLeft()+","+x.getRight()) ;
-        x = dom.giveTile(1);
+        x = dom.giveTile();
         System.out.println(x.getLeft()+","+x.getRight()) ;
     }
 }
--- src/Domino/GameModes/SoloOne/SoloOne.java
+++ src/Domino/GameModes/SoloOne/SoloOne.java
@@ -1,11 +1,15 @@
 package Domino.GameModes.SoloOne;
 
 import Domino.Base.Dominoes;
+import Domino.Base.Tile;
+
+import java.util.ArrayList;
 
 public class SoloOne {
     Dominoes dom;
-
+    ArrayList<ArrayList<Tile>> tiles;
     public SoloOne(){
         dom = new Dominoes();
+        tiles = dom.getGroupofTiles(4,7);
     }
 }
