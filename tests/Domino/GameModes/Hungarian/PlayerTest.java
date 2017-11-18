b931d062285e4815b12b2b8d59b6dbb2eb6d00a3
Christos <christogd@csd.auth.gr>
2017-11-18 07:32:35 +0200

    Import fixes

--- tests/Domino/GameModes/Hungarian/PlayerTest.java
+++ tests/Domino/GameModes/Hungarian/PlayerTest.java
@@ -1,5 +1,5 @@
 package Domino.GameModes.Hungarian;
-
+import Domino.Base.*;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -7,18 +7,50 @@ import static org.junit.Assert.*;
 public class PlayerTest {
     @Test
     public void move() throws Exception {
+        Player p1=new Player();
+        p1.add(new Tile(3,4));
+        p1.add(new Tile(6,6));
+        p1.add(new Tile(1,9));
+        p1.add(new Tile(0,2));
+        p1.add(new Tile(5,7));
+        Tile x=p1.move(2);
+        assertEquals(x.getLeft(),1);
+        assertEquals(x.getRight(),9);
     }
 
     @Test
     public void haveMove() throws Exception {
+        Player p1=new Player();
+        p1.add(new Tile(3,4));
+        p1.add(new Tile(6,6));
+        p1.add(new Tile(1,9));
+        p1.add(new Tile(0,2));
+        p1.add(new Tile(5,7));
+        assertEquals(p1.haveMove(new Tile(2,8),new Tile(4,7)),true);
+        assertEquals(p1.haveMove(new Tile(8,8),new Tile(4,8)),false);
     }
 
     @Test
     public void maxTile() throws Exception {
+        Player p1=new Player();
+        p1.add(new Tile(3,4));
+        p1.add(new Tile(6,6));
+        p1.add(new Tile(1,9));
+        p1.add(new Tile(0,2));
+        p1.add(new Tile(5,7));
+        assertEquals(p1.maxTile().getLeft(),6);
+        assertEquals(p1.maxTile().getRight(),6);
     }
 
     @Test
     public void sumTiles() throws Exception {
+        Player p1=new Player();
+        p1.add(new Tile(3,4));
+        p1.add(new Tile(6,6));
+        p1.add(new Tile(1,9));
+        p1.add(new Tile(0,2));
+        p1.add(new Tile(5,7));
+        assertEquals(p1.sumTiles(),43);
     }
 
 }
