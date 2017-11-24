e0a35e8f6effe785193877c4599fc2591725b340
gkarapii <georgekarapi@yahoo.gr>
2017-11-24 18:42:08 +0200

    Added JavaDoc

--- src/Domino/Base/Tile.java
+++ src/Domino/Base/Tile.java
@@ -14,22 +14,37 @@ public class Tile {
         right = r;
     }
 
+    /**
+     * @return  Get Right number
+     */
     public int getRight() {
         return right;
     }
 
+    /**
+     * @param r Set right int
+     */
     public void setRight(int r) {
         right = r;
     }
 
+    /**
+     * @return  Get Left number
+     */
     public int getLeft() {
         return left;
     }
 
+    /**
+     * @param l Set left int
+     */
     public void setLeft(int l) {
         left = l;
     }
 
+    /**
+     * @return  Total number power of tiles
+     */
     public int getTotal() {
         return left + right;
     }
