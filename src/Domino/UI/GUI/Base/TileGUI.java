7f047e7edba1a522b6b55275a6af9aecc3282606
giorgos <georgekarapi@yahoo.gr>
2017-12-22 17:07:49 +0200

    Testing out Graphics

--- src/Domino/UI/GUI/Base/TileGUI.java
+++ src/Domino/UI/GUI/Base/TileGUI.java
@@ -1,9 +1,39 @@
 package Domino.UI.GUI.Base;
 
-public class TileGUI {
+import javax.swing.*;
+import java.awt.*;
+
+public class TileGUI extends JPanel{
     public int l,r;
-    public TileGUI(int l,int r) {
+    public TileGUI(int l, int r) {
         this.l = l;
         this.r = r;
+    }
+    private void tile(Graphics g){
+        Graphics2D g2d = (Graphics2D) g;
+
+        g2d.setPaint(new Color(150, 150, 150));
+
+        RenderingHints rh = new RenderingHints(
+                RenderingHints.KEY_ANTIALIASING,
+                RenderingHints.VALUE_ANTIALIAS_ON);
+
+        rh.put(RenderingHints.KEY_RENDERING,
+                RenderingHints.VALUE_RENDER_QUALITY);
+
+        g2d.setRenderingHints(rh);
+
+        g2d.fillRect(30, 20, 50, 50);
+        g2d.fillRect(120, 20, 90, 60);
+        g2d.fillRoundRect(250, 20, 70, 60, 25, 25);
+
+        g2d.fillArc(120, 130, 110, 100, 5, 150);
+        g2d.fillOval(270, 130, 50, 50);
+    }
+
+    @Override
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        tile(g);
     }
 }
--- src/Domino/UI/GUI/GUI.java
+++ src/Domino/UI/GUI/GUI.java
@@ -1,16 +1,30 @@
 package Domino.UI.GUI;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class GUI extends JFrame{
     public GUI(){
-
+        setTitle("Domino Game");
+        setSize(512,512);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+        setVisible(true);
     }
     public static void main(String args[]) {
-        JFrame gui = new JFrame("Domino Game");
-        gui.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        gui.pack();
-        gui.setLocationRelativeTo(null);
-        gui.setVisible(true);
+        new GUI();
+    }
+    public void paint(Graphics g){
+        int tileX = 120;
+        int tileY = 60;
+        Graphics2D tile = (Graphics2D) g;
+        Graphics2D dot = (Graphics2D) g;
+        Graphics2D div = (Graphics2D) g;
+        tile.setColor(Color.WHITE);
+        tile.fillRect(40, 40, tileX, tileY);
+        dot.setColor(Color.BLACK);
+        dot.fillOval(40,60,10,10);
+        div.fillRect(100,40,2,60);
+
     }
 }
