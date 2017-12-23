04afdf8f17367e83ffbf60c607c77648df20f543
giorgos <georgekarapi@yahoo.gr>
2017-12-23 13:22:27 +0200

    Semi-final TileGUI

--- src/Domino/UI/GUI/Base/TileGUI.java
+++ src/Domino/UI/GUI/Base/TileGUI.java
@@ -2,38 +2,57 @@ package Domino.UI.GUI.Base;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.geom.Area;
+import java.awt.geom.Ellipse2D;
+import java.awt.geom.Rectangle2D;
 
 public class TileGUI extends JPanel{
-    public int l,r;
+    public int l,r,posX,posY;
     public TileGUI(int l, int r) {
         this.l = l;
         this.r = r;
+        posX = 40;
+        posY = 35;
     }
-    private void tile(Graphics g){
+    private Shape dot(int x, int y) {
+        return new Ellipse2D.Float(x, y, 10, 10);
+    }
+    public void paint(Graphics g){
+        int tileX = 120;
+        int tileY = 60;
+        int rows = 1;
         Graphics2D g2d = (Graphics2D) g;
+        Shape tile = new Rectangle2D.Double(posX, posY, tileX, tileY);
+        Area shape = new Area(tile);
 
-        g2d.setPaint(new Color(150, 150, 150));
-
-        RenderingHints rh = new RenderingHints(
-                RenderingHints.KEY_ANTIALIASING,
-                RenderingHints.VALUE_ANTIALIAS_ON);
-
-        rh.put(RenderingHints.KEY_RENDERING,
-                RenderingHints.VALUE_RENDER_QUALITY);
-
-        g2d.setRenderingHints(rh);
-
-        g2d.fillRect(30, 20, 50, 50);
-        g2d.fillRect(120, 20, 90, 60);
-        g2d.fillRoundRect(250, 20, 70, 60, 25, 25);
-
-        g2d.fillArc(120, 130, 110, 100, 5, 150);
-        g2d.fillOval(270, 130, 50, 50);
-    }
-
-    @Override
-    public void paintComponent(Graphics g) {
-        super.paintComponent(g);
-        tile(g);
+        shape.subtract(new Area(new Rectangle2D.Double(posX + (tileX / 2),posY,2,60)));
+        System.out.println("running");
+        int y = (tileY / 2);
+        if(l > 3){
+            rows = 2;
+            y /= 2;
+        }
+        for(int i = 0; i < rows; i++){
+            for(int j = 0; j <= l; j++){
+                System.out.println(l);
+                if(l == 1 || l == 4){
+                    shape.subtract(new Area(dot((posX - 5) + ((tileX / 2) / 2),y + posY - 5)));
+                    l--;
+                    System.out.println("1st");
+                }
+                else if(l == 2 || l == 5){
+                    shape.subtract(new Area(dot((posX - 5) + (((tileX / 2) / 2) - (((tileX / 2) / 2) / 2)),y + posY - 5)));
+                    l--;
+                    System.out.println("2nd");
+                }
+                else if(l == 3 || l == 6){
+                    shape.subtract(new Area(dot((posX - 5) + (((tileX / 2) / 2) + (((tileX / 2) / 2) / 2)),y + posY - 5)));
+                    l--;
+                    System.out.println("3rd");
+                }
+            }
+            y += (tileY / 2);
+        }
+        g2d.draw(shape);
     }
 }
--- src/Domino/UI/GUI/GUI.java
+++ src/Domino/UI/GUI/GUI.java
@@ -1,30 +1,20 @@
 package Domino.UI.GUI;
 
+import Domino.UI.GUI.Base.TileGUI;
+
 import javax.swing.*;
 import java.awt.*;
 
-public class GUI extends JFrame{
+public class GUI{
     public GUI(){
-        setTitle("Domino Game");
-        setSize(512,512);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setLocationRelativeTo(null);
-        setVisible(true);
+        JFrame frame = new JFrame("Domino Game");
+        frame.setSize(512,512);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLayout(new BorderLayout());
+        frame.add(new TileGUI(6,4));
+        frame.setVisible(true);
     }
     public static void main(String args[]) {
         new GUI();
-    }
-    public void paint(Graphics g){
-        int tileX = 120;
-        int tileY = 60;
-        Graphics2D tile = (Graphics2D) g;
-        Graphics2D dot = (Graphics2D) g;
-        Graphics2D div = (Graphics2D) g;
-        tile.setColor(Color.WHITE);
-        tile.fillRect(40, 40, tileX, tileY);
-        dot.setColor(Color.BLACK);
-        dot.fillOval(40,60,10,10);
-        div.fillRect(100,40,2,60);
-
     }
 }
