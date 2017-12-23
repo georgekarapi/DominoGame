0f124cc14a677d6d6a5d91a15f8266910fd49452
giorgos <georgekarapi@yahoo.gr>
2017-12-23 16:30:30 +0200

    Semi-semi-final TileGUI

--- src/Domino/UI/GUI/Base/TileGUI.java
+++ src/Domino/UI/GUI/Base/TileGUI.java
@@ -5,9 +5,10 @@ import java.awt.*;
 import java.awt.geom.Area;
 import java.awt.geom.Ellipse2D;
 import java.awt.geom.Rectangle2D;
+import java.awt.geom.RoundRectangle2D;
 
 public class TileGUI extends JPanel{
-    public int l,r,posX,posY;
+    private final int l,r,posX,posY;
     public TileGUI(int l, int r) {
         this.l = l;
         this.r = r;
@@ -21,38 +22,49 @@ public class TileGUI extends JPanel{
         int tileX = 120;
         int tileY = 60;
         int rows = 1;
+        int num = l;
+        int posX = this.posX;
+        int posY = this.posY;
         Graphics2D g2d = (Graphics2D) g;
-        Shape tile = new Rectangle2D.Double(posX, posY, tileX, tileY);
+        Graphics2D components = (Graphics2D) g;
+        Shape tile = new RoundRectangle2D.Double(posX, posY, tileX, tileY,10,10);
         Area shape = new Area(tile);
-
-        shape.subtract(new Area(new Rectangle2D.Double(posX + (tileX / 2),posY,2,60)));
+        Area comps = new Area();
+        comps.add(new Area(new Rectangle2D.Double(posX + (tileX / 2),posY,2,60)));
         System.out.println("running");
         int y = (tileY / 2);
         if(l > 3){
             rows = 2;
             y /= 2;
         }
-        for(int i = 0; i < rows; i++){
-            for(int j = 0; j <= l; j++){
-                System.out.println(l);
-                if(l == 1 || l == 4){
-                    shape.subtract(new Area(dot((posX - 5) + ((tileX / 2) / 2),y + posY - 5)));
-                    l--;
-                    System.out.println("1st");
+        for(int s = 0; s < 2; s++) {
+            for (int i = 0; i < rows; i++) {
+                for (int j = 0; j <= l; j++) {
+                    System.out.println(l);
+                    if (num == 3 || num == 6) {
+                        comps.add(new Area(dot((posX - 5) + (((tileX / 2) / 2) + (((tileX / 2) / 2) / 2)), y + posY - 5)));
+                        num--;
+                        System.out.println("1st");
+                    } else if (num == 2 || num == 5) {
+                        comps.add(new Area(dot((posX - 5) + (((tileX / 2) / 2) - (((tileX / 2) / 2) / 2)), y + posY - 5)));
+                        num--;
+                        System.out.println("2nd");
+                    } else if (num == 1 || num == 4) {
+                        comps.add(new Area(dot((posX - 5) + ((tileX / 2) / 2), y + posY - 5)));
+                        num--;
+                        System.out.println("3rd");
+                    }
                 }
-                else if(l == 2 || l == 5){
-                    shape.subtract(new Area(dot((posX - 5) + (((tileX / 2) / 2) - (((tileX / 2) / 2) / 2)),y + posY - 5)));
-                    l--;
-                    System.out.println("2nd");
-                }
-                else if(l == 3 || l == 6){
-                    shape.subtract(new Area(dot((posX - 5) + (((tileX / 2) / 2) + (((tileX / 2) / 2) / 2)),y + posY - 5)));
-                    l--;
-                    System.out.println("3rd");
-                }
+                y += (tileY / 2);
             }
-            y += (tileY / 2);
+            y -= (tileY / 2);
+            posX += (tileX / 2);
+            num = r;
         }
-        g2d.draw(shape);
+        g2d.setColor(Color.WHITE);
+        g2d.fill(shape);
+        components.setColor(Color.BLACK);
+        components.fill(comps);
+        components.drawRoundRect(this.posX, this.posY, tileX, tileY,10,10);
     }
 }
