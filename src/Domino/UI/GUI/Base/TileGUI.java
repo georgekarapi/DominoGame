abde766935ae89d2f43971d83130efa08c697fa3
giorgos <georgekarapi@yahoo.gr>
2017-12-24 14:29:48 +0200

    Final TileGUI!?

--- src/Domino/UI/GUI/Base/TileGUI.java
+++ src/Domino/UI/GUI/Base/TileGUI.java
@@ -8,63 +8,66 @@ import java.awt.geom.Rectangle2D;
 import java.awt.geom.RoundRectangle2D;
 
 public class TileGUI extends JPanel{
-    private final int l,r,posX,posY;
-    public TileGUI(int l, int r) {
+    private final int l,r,posX,posY,width,height,w2,h2;
+    public TileGUI(int l, int r,int width, int height, int posX, int posY) {
         this.l = l;
         this.r = r;
-        posX = 40;
-        posY = 35;
+        this.posX = posX;
+        this.posY = posY;
+        this.width = width;
+        this.height = height;
+        w2 = width/2;
+        h2 = height/2;
     }
     private Shape dot(int x, int y) {
         return new Ellipse2D.Float(x, y, 10, 10);
     }
-    public void paint(Graphics g){
-        int tileX = 120;
-        int tileY = 60;
+    private void createDots(Area comps, int posX, int posY, int num){
         int rows = 1;
-        int num = l;
-        int posX = this.posX;
-        int posY = this.posY;
-        Graphics2D g2d = (Graphics2D) g;
-        Graphics2D components = (Graphics2D) g;
-        Shape tile = new RoundRectangle2D.Double(posX, posY, tileX, tileY,10,10);
-        Area shape = new Area(tile);
-        Area comps = new Area();
-        comps.add(new Area(new Rectangle2D.Double(posX + (tileX / 2),posY,2,60)));
-        System.out.println("running");
-        int y = (tileY / 2);
-        if(l > 3){
+        int y = h2;
+        int tnum = num;
+        if(num > 3){
             rows = 2;
             y /= 2;
+            tnum -= 2;
+            if(num == 6){ tnum--;}
         }
-        for(int s = 0; s < 2; s++) {
-            for (int i = 0; i < rows; i++) {
-                for (int j = 0; j <= l; j++) {
-                    System.out.println(l);
-                    if (num == 3 || num == 6) {
-                        comps.add(new Area(dot((posX - 5) + (((tileX / 2) / 2) + (((tileX / 2) / 2) / 2)), y + posY - 5)));
-                        num--;
-                        System.out.println("1st");
-                    } else if (num == 2 || num == 5) {
-                        comps.add(new Area(dot((posX - 5) + (((tileX / 2) / 2) - (((tileX / 2) / 2) / 2)), y + posY - 5)));
-                        num--;
-                        System.out.println("2nd");
-                    } else if (num == 1 || num == 4) {
-                        comps.add(new Area(dot((posX - 5) + ((tileX / 2) / 2), y + posY - 5)));
-                        num--;
-                        System.out.println("3rd");
-                    }
+        for (int i = 0; i < rows; i++) {
+            if(tnum == 2){
+                comps.add(new Area(dot((posX - 5) + (w2 / 3), y + posY - 5)));
+                comps.add(new Area(dot((posX - 5) + (w2 / 3) + (w2 / 3), y + posY - 5)));
+                tnum -= 2;
+            }else{
+                comps.add(new Area(dot((posX - 5) + (w2 / 2), y + posY - 5)));
+                if(tnum == 3){
+                    comps.add(new Area(dot((posX - 5) + ((w2 / 2) - ((w2 / 2) / 2)), y + posY - 5)));
+                    comps.add(new Area(dot((posX - 5) + ((w2 / 2) + ((w2 / 2) / 2)), y + posY - 5)));
+                    tnum -= 2;
                 }
-                y += (tileY / 2);
+                tnum--;
             }
-            y -= (tileY / 2);
-            posX += (tileX / 2);
-            num = r;
+            y += h2;
+            if(num > 3){
+                tnum += 2;
+                if(num == 6){ tnum++;}
+            }
         }
+    }
+    public void paint(Graphics g){
+        Graphics2D g2d = (Graphics2D) g;
+        Graphics2D components = (Graphics2D) g;
+        Shape tile = new RoundRectangle2D.Double(posX, posY, width, height,10,10);
+        Area shape = new Area(tile);
+        Area comps = new Area();
+        comps.add(new Area(new Rectangle2D.Double(posX + w2,posY,2,60)));
+
+        createDots(comps, posX, posY, l);
+        createDots(comps, posX + w2, posY, r);
+
         g2d.setColor(Color.WHITE);
         g2d.fill(shape);
         components.setColor(Color.BLACK);
         components.fill(comps);
-        components.drawRoundRect(this.posX, this.posY, tileX, tileY,10,10);
+        components.drawRoundRect(this.posX, this.posY, width, height,10,10);
     }
 }
