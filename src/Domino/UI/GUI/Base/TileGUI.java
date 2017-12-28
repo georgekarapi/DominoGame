9bff3abd7238c3bf874c4b023894f2069aa2f7be
giorgos <georgekarapi@yahoo.gr>
2017-12-28 14:12:50 +0200

    Fixed TileGUI scaling

--- src/Domino/UI/GUI/Base/TileGUI.java
+++ src/Domino/UI/GUI/Base/TileGUI.java
@@ -9,18 +9,18 @@ import java.awt.geom.RoundRectangle2D;
 
 public class TileGUI extends JPanel{
     private final int l,r,posX,posY,width,height,w2,h2;
-    public TileGUI(int l, int r,int width, int height, int posX, int posY) {
+    public TileGUI(int l, int r,int width, int posX, int posY) {
         this.l = l;
         this.r = r;
         this.posX = posX;
         this.posY = posY;
         this.width = width;
-        this.height = height;
+        this.height = width / 2;
         w2 = width/2;
         h2 = height/2;
     }
-    private Shape dot(int x, int y) {
-        return new Ellipse2D.Float(x, y, 10, 10);
+    private Shape dot(float x, float y) {
+        return new Ellipse2D.Float(x, y, height / 6, height / 6);
     }
     private void createDots(Area comps, int posX, int posY, int num){
         int rows = 1;
@@ -28,25 +28,25 @@ public class TileGUI extends JPanel{
         int tnum = num;
         if(num > 3){
             rows = 2;
-            y -= 10;
+            y -= (height / 6);
             tnum -= 2;
             if(num == 6){ tnum--;}
         }
         for (int i = 0; i < rows; i++) {
             if(tnum == 2){
-                comps.add(new Area(dot((posX - 5) + (w2 / 3), y + posY - 5)));
-                comps.add(new Area(dot((posX - 5) + (w2 / 3) + (w2 / 3), y + posY - 5)));
+                comps.add(new Area(dot((posX - (height / 6) / 2) + (w2 / 3), y + posY - (height / 6) / 2)));
+                comps.add(new Area(dot((posX - (height / 6) / 2) + (w2 / 3) + (w2 / 3), y + posY - (height / 6) / 2)));
                 tnum -= 2;
             }else{
-                comps.add(new Area(dot((posX - 5) + (w2 / 2), y + posY - 5)));
+                comps.add(new Area(dot((posX - (height / 6) / 2) + (w2 / 2), y + posY - (height / 6) / 2)));
                 if(tnum == 3){
-                    comps.add(new Area(dot((posX - 5) + ((w2 / 2) - ((w2 / 2) / 2)), y + posY - 5)));
-                    comps.add(new Area(dot((posX - 5) + ((w2 / 2) + ((w2 / 2) / 2)), y + posY - 5)));
+                    comps.add(new Area(dot((posX - (height / 6) / 2) + ((w2 / 2) - ((w2 / 2) / 2)), y + posY - (height / 6) / 2)));
+                    comps.add(new Area(dot((posX - (height / 6) / 2) + ((w2 / 2) + ((w2 / 2) / 2)), y + posY - (height / 6) / 2)));
                     tnum -= 2;
                 }
                 tnum--;
             }
-            y += 20;
+            y += (height / 3);
             if(num > 3){
                 tnum += 2;
                 if(num == 6){ tnum++;}
@@ -56,10 +56,10 @@ public class TileGUI extends JPanel{
     public void paint(Graphics g){
         Graphics2D g2d = (Graphics2D) g;
         Graphics2D components = (Graphics2D) g;
-        Shape tile = new RoundRectangle2D.Double(posX, posY, width, height,10,10);
+        Shape tile = new RoundRectangle2D.Double(posX, posY, width, height,height / 6,height / 6);
         Area shape = new Area(tile);
         Area comps = new Area();
-        comps.add(new Area(new Rectangle2D.Double(posX + w2,posY,2,60)));
+        comps.add(new Area(new Rectangle2D.Double(posX + w2,posY,2,height)));
 
         createDots(comps, posX, posY, l);
         createDots(comps, posX + w2, posY, r);
@@ -68,6 +68,6 @@ public class TileGUI extends JPanel{
         g2d.fill(shape);
         components.setColor(Color.BLACK);
         components.fill(comps);
-        components.drawRoundRect(this.posX, this.posY, width, height,10,10);
+        components.drawRoundRect(this.posX, this.posY, width, height,height / 6,height / 6);
     }
 }
