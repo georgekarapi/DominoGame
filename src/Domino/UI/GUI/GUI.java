8aa2fe4641604ea996251ee83634a022e8979b21
giorgos <georgekarapi@yahoo.gr>
2018-01-04 19:11:57 +0200

    Finalized DraggableImage

--- src/Domino/UI/GUI/GUI.java
+++ src/Domino/UI/GUI/GUI.java
@@ -6,19 +6,25 @@ import javax.swing.*;
 import java.awt.*;
 
 public class GUI{
-    DraggableImage tile = new DraggableImage(2, 6, 100, true);
+    DraggableImage tile = new DraggableImage(2, 6, 100, false);
 
     //TileGUI tile2 = new TileGUI(1,3,100);
     public GUI(){
         JFrame frame = new JFrame("Domino Game");
         frame.setSize(512,512);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setLayout(new GridLayout(0, 1));
+        frame.setLayout(new BorderLayout());
+        JLayeredPane jLayeredPane = frame.getLayeredPane();
+        jLayeredPane.setLayout(new GridLayout(0, 1));
         JPanel jPanel = new JPanel();
-        ///jPanel.add(new JButton());
-        //jPanel.add(tile);
-        tile.rotate();
-        frame.add(tile);
+        JPanel jPanel1 = new JPanel();
+        jPanel.setBorder(BorderFactory.createTitledBorder(
+                "Panel 1"));
+        jPanel1.setBorder(BorderFactory.createTitledBorder(
+                "Panel 2"));
+        jPanel.add(tile);
+        jLayeredPane.add(jPanel, JLayeredPane.DEFAULT_LAYER);
+        jLayeredPane.add(jPanel1, JLayeredPane.DEFAULT_LAYER);
         frame.setVisible(true);
     }
 
